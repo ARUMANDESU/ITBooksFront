b06cda55aa02437eff11dbb6438a3e00c57da90a
arumandesu <zh.arumandes@gmail.com>
2023-02-03 20:23:22 +0600

    search bar

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
     return (
         <div>
             <Link to={`/`}>Homepage</Link> <Link to={`/auth/login`}>Login</Link>{" "}
             <Link to={`/auth/register`}>Register</Link>
+            {"      "}
+            <SearchBar />
         </div>
     );
 };
--- src/pages/components/SearchBar.tsx
+++ src/pages/components/SearchBar.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "../../hooks/debounce";
+import axios from "axios";
+import { IBook, url } from "../../models/types";
+import { Link } from "react-router-dom";
 
 const SearchBar = () => {
-    return <div></div>;
+    const [search, setSearch] = useState("");
+    const [books, setBooks] = useState([] as IBook[]);
+    const debounced = useDebounce(search);
+    useEffect(() => {
+        if (debounced.length > 3) {
+            const data = { payload: debounced };
+            axios.post(`${url}/book/search`, data).then((res) => {
+                setBooks(res.data);
+            });
+        }
+    }, [debounced]);
+    return (
+        <div>
+            <input
+                type="search"
+                placeholder="Search"
+                onChange={(e) => {
+                    setSearch(e.target.value);
+                }}
+            />
+            <div style={{ overflowY: "scroll", height: "250px" }}>
+                {books.map((book) => (
+                    <>
+                        <Link to={`/book/${book._id}`}>
+                            <h5>{book.title}</h5>
+                        </Link>
+                    </>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default SearchBar;
